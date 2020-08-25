8ebf5cd7fb8f8a017b9e4addc065bec57239222b
Pedro Alcalde <palcalde02@gmail.com>
2020-08-25 19:15:52 -0400

    inicio de sesión guarda usuario y clave

--- ventana.py
+++ ventana.py
@@ -1,16 +1,53 @@
 import tkinter
+from tkinter import messagebox
+from tkinter import ttk
 import serpiente
+import sqlite3
 
 root = tkinter.Tk()
-root.title("la serpiente")
+root.geometry('300x100')
+root.title('la serpiente')
 
 frame = tkinter.Frame(root)
 frame.pack()
-frame.config(width=500, height=200)
 
-
+# Variables---------------------------------------------------------------------
 nombre_usuario = tkinter.StringVar()
 nombre_usuario.set('')
+texto_contraseña = tkinter.StringVar()
+texto_contraseña.set('')
+sesion = False
+
+# Funciones---------------------------------------------------------------------
+def inicio_sesion():
+    conexion = sqlite3.connect('datos usuarios')
+    cursor = conexion.cursor()
+    cursor.execute("INSERT INTO DatosUsuarios (Usuario, Contraseña) VALUES ('{}', '{}')".format(nombre_usuario.get(), texto_contraseña.get()))
+    conexion.commit()
+    global sesion
+    sesion = True
+    root.title('a serpiente - ' + nombre_usuario.get())
+    messagebox.showinfo('Sesión iniciada', 'Juega nomás. Besos en el potito')
+    conexion.close()
+
+def iniciar_sesion():
+    usuario = tkinter.Label(frame, text='Nombre de usuario')
+    usuario.grid(row=0, column=0, padx=5, pady=5)
+    usuario_input = tkinter.Entry(frame, textvariable=nombre_usuario)
+    usuario_input.grid(row=0, column=1, padx=5, pady=5)
+    contraseña = tkinter.Label(frame, text='Contraseña')
+    contraseña.grid(row=1, column=0, padx=5, pady=5)
+    contraseña_input = tkinter.Entry(frame, textvariable=texto_contraseña)
+    contraseña_input.grid(row=1, column=1, padx=5, pady=5)
+    boton_guardar = tkinter.Button(frame, text='Guardar usuario', command=inicio_sesion)
+    boton_guardar.grid(row=2, column=0, padx=5, pady=5)
+
+def jugar():
+    if sesion:
+        serpiente.juego()
+    else:
+        messagebox.showinfo('Avíspate', 'Inicia sesión para poder jugar')
+        iniciar_sesion()
 
 
 #  MENÚ-------------------------------------------------------------------------
@@ -23,7 +60,10 @@ configuracion.add_command(label='obstaculos')
 configuracion.add_command(label='porte de los puntos')
 
 jugador = tkinter.Menu(menu_juego, tearoff=0)
-jugador.add_command(label='cambiar jugador')
+if sesion:
+    accion_sesion = 'Cerrar sesión'
+else:
+    jugador.add_command(label='Iniciar sesión', command=iniciar_sesion)
 
 vista = tkinter.Menu(menu_juego, tearoff=0)
 vista.add_command(label='claro')
@@ -40,17 +80,8 @@ menu_juego.add_cascade(label='salir', menu=salir)
 
 
 # WIDGETS-----------------------------------------------------------------------
-usuario = tkinter.Label(frame, text='Nombre de usuario')
-usuario.grid(row=0, column=0, padx=10, pady=10)
-
-usuario_input = tkinter.Entry(frame, textvariable=nombre_usuario)
-usuario_input.grid(row=0, column=1, padx=10, pady=10)
-
-
-
-boton_jugar = tkinter.Button(frame, text="jugar!", command=serpiente.juego, cursor="hand2")
-boton_jugar.grid(row=2, column=1, padx=10, pady=10)
-
+boton = tkinter.Button(frame, text='Jugar!', command=jugar, cursor="hand2")
+boton.grid(row=2, column=1, padx=10, pady=10)
 
 
 root.mainloop()
